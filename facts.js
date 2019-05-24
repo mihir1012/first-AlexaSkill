ed73f73845b8956b6a70f4c21b4aaf07d5851619
mihir1012 <45787268+mihir1012@users.noreply.github.com>
2019-05-24 18:06:08 +0530

    Create the file 1

---
+++ facts.js
@@ -0,0 +1,36 @@
+
+
+module.exports.IPL_FACTS=[
+
+	"Broadcasting rights of IPL 2018 are sold for a whopping ₹16347.5 crore to Star India Network. It means 1 ball costs around 21 Lakh Rupees",
+
+	" In IPL, Delhi Daredevils is the only team that had never played an IPL Final",
+
+	"Praveen Kumar had bowled the highest number of dot balls in the history of IPL. He has bowled 1075 dot balls in 119 Matches",
+
+	"Interestingly, Praveen Kumar is also the bowler who has bowled the first-ever over in IPL",
+
+	"Praveen Kumar is the bowler who has bowled the most number of maiden overs in the history of IPL. He has bowled 14 maiden overs",
+	
+	"Chennai Super Kings is the only team in IPL that has never changed its captain until this Wednesday.",
+	
+	"Amit Mishra holds the record of highest number of hat-tricks (3) in the history of IPL.",
+	
+	"Virat Kohli holds the record of most runs in one IPL Season. He has made 973 runs in IPL 2016.",
+	
+	"In every IPL Season, RCB promotes “Go Green” initiative by wearing green jersey for a match",
+	
+	"Ishant Sharma holds the record of conceding most runs in an innings. He conceded 66 runs in 4 overs against CSK in IPL 2013",
+	
+	"Aged 45, Brad Hogg is the oldest player in the history of IPL",
+	
+	"Instead of a two-year suspension from the IPL, CSK has reached the final 6 times, most by any team in the history of IPL",
+	
+
+
+
+
+]
+
+
+
